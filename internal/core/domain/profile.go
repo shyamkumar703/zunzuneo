af353bb1100352de2678b76c0fc2cbce8613b9d7
Shyam Kumar <shyam.kumar@volleythat.com>
2025-03-11 12:11:10 -0700

    added random profile generation using llm

--- internal/core/domain/profile.go
+++ internal/core/domain/profile.go
@@ -34,8 +34,17 @@ func CreateRandomPersonality() Personality {
 	}
 }
 
+type Gender int
+
+const (
+	Male Gender = iota
+	Female
+)
+
 type Profile struct {
 	ID          string      `json:"id"`
+	Gender      Gender      `json:"gender"`
+	City        string      `json:"city"`
 	Handle      string      `json:"handle"`
 	DisplayName string      `json:"displayName"`
 	Bio         string      `json:"bio"`
@@ -49,39 +58,54 @@ type Profile struct {
 func CreateRandomProfile(ctx *context.Context, prompt string) (*Profile, error) {
 	personality := CreateRandomPersonality()
 	id := uuid.New().String()
+	randomBool := rand.Intn(2) == 1
+	var gender Gender
+	var genderString string
+	if randomBool {
+		gender = Male
+		genderString = "male"
+	} else {
+		gender = Female
+		genderString = "female"
+	}
 	type GeneratedProfileResponse struct {
 		Handle      string     `json:"handle"`
 		DisplayName string     `json:"displayName"`
+		City        string     `json:"city"`
 		Bio         string     `json:"bio"`
 		Interests   []Interest `json:"interests"`
 	}
 	formattedPrompt := fmt.Sprintf(
 		`
-		You are generating a Twitter profile for a user who lives in a world just like the one you know, but
-		with some fictional elements. Here are the fictional elements: %s. Here are the user's big 5 personality
-		traits: %+v. You need to generate:
+		You are generating a Twitter profile for an NPC in an NBA simulation game. There is a 60%% chance this user is NOT an NBA fan. If, based on the context you've
+		been given and some element of random chance, this user would happen to be an NBA fan, use this background information
+		about the state of the NBA simulation to inform your decision making about the user's profile: %s. Here are the user's big
+		5 personality traits as well: %+v. And here is the user's gender: %s. You need to generate:
 		1) the user's display name. Generate a name for this user, this can include both a first name and a last name, just
 		a first name, just a last name, even a first name, middle name, and a last name. Try to make the name as interesting and as
 		realistic as possible. THIS DOES NOT HAVE TO BE THE USER'S REAL NAME. Think of this more so as an extension of the display name. If the user's personality
 		traits indicate that they may use their real name as their display name on Twitter however, make it their real name. You DO NOT
 		have to worry about capitalization rules that would normally apply to names, you can mix in all lowercase and all uppercase names.
-		2) the user's interests. An interest needs to be a short, specific topic that a random Twitter user may be interested in. For example, instead
+		2) the city the user lives in. This must be within the United States.
+		3) the user's interests. An interest needs to be a short, specific topic that a random Twitter user may be interested in. For example, instead
 		of a user being interested in basketball, try to pinpoint a particular team, like the New York Knicks or the Golden State Warriors. In conjuction with
 		generating an interest, generate a corresponding interest level; a floating point value between -1 and 1 that indicates the user's attitude towards the
 		interest. A low value (closer to -1) will indicate a negative interest in the topic (the user is interested insofar as they do not like that topic), and a high value
 		(closer to 1) will indicate a positive interest in the topic (the user likes and enjoys the topic). Allow these to build on top of each other - for example,
 		if a user has a high positive interest in the San Francisco Giants, they should have a significant negative interest in the Los Angeles Dodgers, as those two teams
 		are rivals. Generate at least 40 of these topics, and make them as specific as possible. Some of them should be political, some of them should be hobby-related, some of
-		them should be job-related. Do not generate something like "music festivals", instead generate "Coachella". Prefer specific proper nouns over overarching topics.
-		3) the user's Twitter handle. Try to keep this short, include numbers and punctuation, lean into anything that could
+		them should be job-related. Do not generate something like "music festivals", instead generate "Coachella". Prefer specific proper nouns over overarching topics. Factor
+		in the city the user lives in, and their gender.
+		4) the user's Twitter handle. Try to keep this short, include numbers and punctuation, lean into anything that could
 		be potentially funny, avoid any direct references to the big 5 personality traits but use them to inform your generation, incorporate the user's interests, incorporate
 		the user's display name if necessary.
-		4) the user's Twitter bio. Try to keep this to roughly 2-4 sentences, reference the user's interests in a way that would align with their personality traits.
+		5) the user's Twitter bio. Try to keep this to roughly 2-4 sentences, reference the user's interests in a way that would align with their personality traits.
 		This needs to be outputted in the JSON format below:
 		{
 			handle: string, // warriorsfan22
-			displayName: string // frosty
-			bio: string // die hard warriors fan. FUCK THE KINGS
+			displayName: string, // frosty
+			city: string, // San Francisco
+			bio: string, // die hard warriors fan. FUCK THE KINGS
 			interests: Interest[] // [{interest: 'Golden State Warriors', interestLevel: 0.94}]
 		}
 
@@ -93,6 +117,7 @@ func CreateRandomProfile(ctx *context.Context, prompt string) (*Profile, error) 
 		`,
 		prompt,
 		personality,
+		genderString,
 	)
 	response, err := dependencies.RequestLLM(formattedPrompt, ctx)
 	if err != nil {
@@ -107,6 +132,8 @@ func CreateRandomProfile(ctx *context.Context, prompt string) (*Profile, error) 
 	}
 	profile := &Profile{
 		ID:          id,
+		Gender:      gender,
+		City:        profileResponse.City,
 		Handle:      profileResponse.Handle,
 		DisplayName: profileResponse.DisplayName,
 		Bio:         profileResponse.Bio,
--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"zunzuneo/internal/core/domain"
 	"zunzuneo/internal/dependencies"
 
 	"github.com/joho/godotenv"
@@ -17,13 +15,4 @@ func main() {
 	if depErr != nil {
 		panic(depErr)
 	}
-	backgroundPrompt := `
-	The year is 2029. Not much advancement has occurred in the world past your knowledge cutoff. Notably, in the NBA, the Oklahoma City Thunder
-	have won the last 3 NBA championships.
-	`
-	profile, err := domain.CreateRandomProfile(nil, backgroundPrompt)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v\n", profile)
 }
